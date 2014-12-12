fb485e514b7bca588faa89fed4c315ef85812a76
Famouscake <peter.g.hristov@gmail.com>
2014-12-12 20:34:23 +0000

    added initial seed

--- community-house/Migrations/Configuration.cs
+++ community-house/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace community_house.Migrations
 {
+    using community_house.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,6 +16,30 @@ namespace community_house.Migrations
 
         protected override void Seed(community_house.Models.ApplicationDbContext context)
         {
+            List<House> Houses = new List<House>
+            {
+                new House{Address = "1 Shire Oak Road", Area = 55.5, City = "Pleven", Price = 50000, UserId = "da681c1a-d246-44c8-ba2f-9a6a351ce83f"},
+
+                new House{Address = "21 Cliffside Heaven", Area = 155.5, City = "Varna", Price = 100000, UserId = "da681c1a-d246-44c8-ba2f-9a6a351ce83f"},
+
+                new House{Address = "19 Something", Area = 12.5, City = "Kaspichan", Price = 1000, UserId = "da681c1a-d246-44c8-ba2f-9a6a351ce83f"},
+            };
+
+            Houses.ForEach(s => context.Houses.Add(s));
+            context.SaveChanges();
+
+            List<Picture> Pictures = new List<Picture>
+            {
+                new Picture{FileName = "test.png", HouseID = 1},
+                new Picture{FileName = "test.png", HouseID = 2},
+                new Picture{FileName = "test.png", HouseID = 3}
+            };
+
+            Pictures.ForEach(s => context.Pictures.Add(s));
+            context.SaveChanges();
+
+
+            //context.Pictures.AddOrUpdate(new Models.Picture { });
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
